7bae8279f4ba09da57d075af10c8ea60073a8b15
Shane Yu <shaneyu07@gmail.com>
2017-07-19 13:30:12 +0800

    added column look_times

--- momoCrawler.py
+++ momoCrawler.py
@@ -42,7 +42,10 @@ class momo(object):
 		self.cookies2 = {
 			'_ts_id': '888888888888888888',
 		}
-		self.result_df = pd.DataFrame(columns=('GID', 'price', 'discount', 'payment_CreditCard', 'payment_Arrival', 'payment_ConvenienceStore', 'payment_ATM', 'payment_iBon', 'preferential_count', 'img_height', 'is_warm', 'is_cold', 'is_bright', 'is_dark', '12H', 'shopcart', 'superstore', 'productFormatCount', 'attributesListArea', 'label'))
+		self.result_df = pd.DataFrame(columns=('GID', 'price', 'discount', 'payment_CreditCard', \
+			'payment_Arrival', 'payment_ConvenienceStore', 'payment_ATM', 'payment_iBon', \
+			'preferential_count', 'img_height', 'is_warm', 'is_cold', 'is_bright', 'is_dark', \
+			'12H', 'shopcart', 'superstore', 'productFormatCount', 'attributesListArea', 'look_times', 'label'))
 		if status == 'c':
 			self.with_header = False
 		elif status == 'i':
@@ -230,7 +233,7 @@ class momo(object):
 			return 0
 	
 	@timeit
-	def get_rows(self, goods_icode, label):
+	def get_rows(self, goods_icode, look_num, label):
 		web = 'https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=' + goods_icode
 		time.sleep(1)
 		h = requests.get(web, headers=self.headers, cookies=self.cookies)
@@ -251,12 +254,13 @@ class momo(object):
 		row_list += self.transport(soup)
 		row_list.append(self.productFormatCount(soup))
 		row_list.append(self.attributesListArea(soup))
+		row_list.append(look_num)
 		row_list.append(label)
 		print row_list
 		return row_list
 
-	def create_csv(self):
-		gid_list = pd.read_csv('./girlshoes7899.csv').values
+	def create_csv(self, input_file_name, output_file_name):
+		gid_list = pd.read_csv(input_file_name).values
 		requests_count = 0
 		successful = 0
 		abandoned = 0
@@ -267,22 +271,22 @@ class momo(object):
 			# if requests_count == 10: break
 			print str(int(row[0])), row[1]
 			try:
-				self.result_df.loc[0] = self.get_rows(str(int(row[0])), row[1])
+				self.result_df.loc[0] = self.get_rows(str(int(row[0])), row[3], row[1])
 				if first_write and self.with_header:
-					self.result_df.to_csv('./result_girlshoes_7899.csv', mode='a', index=False)
+					self.result_df.to_csv(output_file_name, mode='a', index=False)
 					first_write = False
 					self.with_header = False
 				else:
-					self.result_df.to_csv('./result_girlshoes_7899.csv', mode='a', index=False, header=False)
+					self.result_df.to_csv(output_file_name, mode='a', index=False, header=False)
 				successful += 1
-				print '已requests數: ', str(requests_count)+'/7899'
+				print '已requests數: ', str(requests_count)
 				print '已有資料筆數: ', successful
 
 			except Exception, e:
 				abandoned += 1
 				print '爬不到，抱歉'
 				print e
-				print '已requests數: ', str(requests_count)+'/7899'
+				print '已requests數: ', str(requests_count)
 				print '已有資料筆數: ', successful
 				continue
 
@@ -297,7 +301,7 @@ if __name__ == '__main__':
 	obj = momo(sys.argv[1])
 
 	start = time.time()
-	obj.create_csv()
+	obj.create_csv(sys.argv[2], sys.argv[3])
 	end = time.time()
 
 	time_cost = end - start
